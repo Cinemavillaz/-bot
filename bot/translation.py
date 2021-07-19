a76fc184f7daed21ecef737fffaeeabbd00cf80a
Cinemavillaz <87010768+Cinemavillaz@users.noreply.github.com>
2021-07-19 12:46:20 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,13 +4,13 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+    START_TEXT = """<b>Hey {}എന്തുണ്ട് !!</b>
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+<i>ഞാൻ ഒരു ഓട്ടോഫിൽറ്റർ ബോട്ട് ആണ്....😉
 
-For More Details Click Help Button Below..
-@CrazyBotsz
+I Am Being Maintained By @youcant_call_Me_leo 😀
+
+Join @cv_group1 
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +43,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@cv_songs
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/youcant_call_Me_leo">𝑳𝑰𝑶𝑵𝑬𝑳 𝑴𝒆𝑺𝑺𝑰</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot"></a></i>
 """
